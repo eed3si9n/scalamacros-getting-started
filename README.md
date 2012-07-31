46a958e0c492aa2ee6664392905ff7cc2a93acba
e.e d3si9n <eed3si9n@gmail.com>
2012-07-31 19:20:54 -0400

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+Taken from http://scalamacros.org/documentation/gettingstarted.html.
+
+To run this, launch sbt 0.11.3:
+
+    > project app
+    > clean
+    > run
---
+++ project/build.scala
@@ -0,0 +1,26 @@
+import sbt._
+
+object Builds extends Build {
+  import Keys._
+
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    version := "0.1.0",
+    organization := "com.example",
+    scalaVersion := "2.10.0-M6"
+  )
+
+  lazy val root = Project("root", file("."),
+    settings = buildSettings ++ Seq(
+      name := "macro")) aggregate(library)
+  lazy val library = Project("library", file("library"),
+    settings = buildSettings ++ Seq(
+      name := "macro-library",
+      scalacOptions += "-language:experimental.macros",
+      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
+    ))
+  lazy val app = Project("app", file("app"),
+    settings = buildSettings ++ Seq(
+      name := "macro-app"
+      ,scalacOptions += "-Ymacro-debug-lite"
+    )) dependsOn(library)
+}
